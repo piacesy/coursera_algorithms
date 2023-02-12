d64213557b97083d9916cb3a60a9831bda83e1ea
piacesy <2319008062@qq.com>
2023-02-12 17:03:01 +0800

    第一次作业

---
+++ week0/src/homework0/HelloGoodbye.java
@@ -0,0 +1,10 @@
+package homework0;
+
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        String p1=args[0];
+        String p2=args[1];
+        System.out.println("Hello "+p1+" and "+p2);
+        System.out.println("Goodbye "+p2+" and "+p1);
+    }
+}
