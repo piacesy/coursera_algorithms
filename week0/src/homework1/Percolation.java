3fc4edaa02e552cc8affb22dcaae68e64b391ad8
piacesy <2319008062@qq.com>
2023-02-13 10:28:53 +0800

    修正蒙特卡罗模拟

--- week0/src/homework1/Percolation.java
+++ week0/src/homework1/Percolation.java
@@ -12,45 +12,80 @@ public class Percolation {
     private final int top;
     private final int bottom;
 
-    public Percolation(int n){
-        if(n <= 0){
+    public Percolation(int n) {
+        if (n <= 0) {
             throw new IllegalArgumentException("n should be a positive number.");
         }
         grid = new boolean[n][n];
-        union0 = new WeightedQuickUnionUF(n*n+2);
-        union1 = new WeightedQuickUnionUF(n*n+2);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = false;
+            }
+        }
+        union0 = new WeightedQuickUnionUF(n * n + 2);
+        union1 = new WeightedQuickUnionUF(n * n + 1);
         count = 0;
         this.n = n;
         top = 0;
-        bottom = n*n+1;
+        bottom = n * n + 1;
     }
-    private int indexOf(int row,int col){
-        return row*n+col+1;
+
+    private int indexOf(int row, int col) {
+        return (row - 1) * n + col;
     }
-    private void isValid(int row,int col){
-        if(row>=n||row<0||col>=n||col<0){
+
+    private void isValid(int row, int col) {
+        if (row > n || row < 1 || col > n || col < 1) {
             throw new IllegalArgumentException("The number is out of the border.");
         }
     }
-    public void open(int row,int col){
-        isValid(row,col);
-        if(!isOpen(row,col)){
-            grid[row][col]=true;
+
+    public void open(int row, int col) {
+        isValid(row, col);
+        if (!isOpen(row, col)) {
+            grid[row - 1][col - 1] = true;
             count++;
+            if (row == 1) {
+                union0.union(indexOf(row, col), top);
+                union1.union(indexOf(row, col), top);
+            }
+            if (row == n) {
+                union0.union(indexOf(row, col), bottom);
+            }
+            if (row > 1 && isOpen(row - 1, col)) {
+                union0.union(indexOf(row, col), indexOf(row - 1, col));
+                union1.union(indexOf(row, col), indexOf(row - 1, col));
+            }
+            if (row < n && isOpen(row + 1, col)) {
+                union0.union(indexOf(row, col), indexOf(row + 1, col));
+                union1.union(indexOf(row, col), indexOf(row + 1, col));
+            }
+            if (col > 1 && isOpen(row, col - 1)) {
+                union0.union(indexOf(row, col), indexOf(row, col - 1));
+                union1.union(indexOf(row, col), indexOf(row, col - 1));
+            }
+            if (col < n && isOpen(row, col + 1)) {
+                union0.union(indexOf(row, col), indexOf(row, col + 1));
+                union1.union(indexOf(row, col), indexOf(row, col + 1));
+            }
         }
     }
-    public boolean isOpen(int row,int col){
-        isValid(row,col);
-        return grid[row][col];
+
+    public boolean isOpen(int row, int col) {
+        isValid(row, col);
+        return grid[row - 1][col - 1];
     }
-    public boolean isFull(int row,int col){
-        isValid(row,col);
-        return union1.find(top)==union1.find(indexOf(row,col));
+
+    public boolean isFull(int row, int col) {
+        isValid(row, col);
+        return union1.find(top) == union1.find(indexOf(row, col));
     }
-    public int numberOfOpenSites(){
+
+    public int numberOfOpenSites() {
         return count;
     }
-    public boolean percolates(){
-        return union0.find(top)==union0.find(bottom);
+
+    public boolean percolates() {
+        return union0.find(top) == union0.find(bottom);
     }
 }
